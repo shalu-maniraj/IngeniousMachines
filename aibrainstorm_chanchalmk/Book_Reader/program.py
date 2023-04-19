448e0ab8cd121c86f178145bd6c195149bf4d8b2
chanchalmk <73247263+chanchalmk@users.noreply.github.com>
2023-04-19 10:53:47 +0530

    Delete program.py

--- aibrainstorm_chanchalmk/Book_Reader/program.py
+++
@@ -1,9 +0,0 @@
-import io
-from gtts import gTTS
-from playsound import playsound
-fp=io.open("book.txt",mode="r",encoding="utf-8")
-book_content = fp.read()
-ob = gTTS(book_content,lang='ml')
-ob.save("book.mp3")
-playsound("book.mp3")
-print("Successfully reading the book")
